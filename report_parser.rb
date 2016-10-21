9c6a33ac05a429413120037db8ac3c35fde68822
Scott Dixon & Marissa Traina & Melanie Gershman <pair+dixonscottr.marissatraina.melaniegershman@devbootcamp.com>
2016-10-21 11:34:40 -0400

    Refactor report parser

--- report_parser.rb
+++ report_parser.rb
@@ -6,12 +6,11 @@ module ReportParser
 
   NYC_DOH_URL = 'http://data.cityofnewyork.us/resource/43nn-pn8j.json'
 
-  def self.parse(filename)
+  def self.parse(filename, report_binder, klass)
     file = open(filename).read
     report_hash_array = JSON.parse(file)
     report_hash_array.each do |report|
-      # report_binder.reports <<
-      RestaurantReport.new(report)
+      report_binder.reports << klass.new(report)
     end
   end
 end
