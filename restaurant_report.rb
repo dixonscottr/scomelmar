526cc50ffc132586d2e6c1c8a8c1e52302b7c251
Scott Dixon & Marissa Traina & Melanie Gershman <pair+dixonscottr.marissatraina.melaniegershman@devbootcamp.com>
2016-10-21 10:46:52 -0400

    Create restaurant report class

---
+++ restaurant_report.rb
@@ -0,0 +1,22 @@
+class RestaurantReport
+  def initialize(args = {})
+    @cuisine_description = args.fetch("cuisine_description", "")
+    @dba = args.fetch("dba", "")
+    @record_date = args.fetch("record_date", "")
+    @boro = args.fetch("boro", "")
+    @inspection_date = args.fetch("inspection_date", "")
+    @building = args.fetch("building", "")
+    @zipcode = args.fetch("zipcode", "")
+    @score = args.fetch("score", "")
+    @phone = args.fetch("phone", "")
+    @street = args.fetch("street", "")
+    @grade = args.fetch("grade", "")
+    @critical_flag = args.fetch("critical_flag", "")
+    @camis = args.fetch("camis", "")
+    @action = args.fetch("action", "")
+    @violation_code = args.fetch("violation_code", "")
+    @violation_description = args.fetch("violation_description", "")
+    @grade_date = args.fetch("grade_date", "")
+    @inspection_type = args.fetch("inspection_type", "")
+  end
+end
