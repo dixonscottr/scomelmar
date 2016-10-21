9082dafdf44955e77a8884d0979d58c6b98ca3b0
Scott Dixon & Marissa Traina & Melanie Gershman <pair+dixonscottr.marissatraina.melaniegershman@devbootcamp.com>
2016-10-21 14:44:04 -0400

    Fix report parser by removing comma from attr_reader, change keys to symbols

--- report_parser.rb
+++ report_parser.rb
@@ -7,25 +7,7 @@ module ReportParser
     file = open(filename).read
     report_hash_array = JSON.parse(file)
     report_hash_array.each do |report|
-      binder.reports << RestaurantReport.new(restaurant)
+      binder.reports << klass.new(report)
     end
   end
 end
-
-#----Driver-------
-klass = RestaurantReport
-binder = ReportBinder.new
-url = "http://data.cityofnewyork.us/resource/43nn-pn8j.json?zipcode=11211"
-ReportParser.parse(url, binder, klass)
-# zip_query = "zipcode="
-
-# puts "what is the zipcode you want to search for?"
-# user_input = gets.chomp
-
-# json_string_response = open(url + "?" + zip_query + user_input).read
-
-# ruby_hash_response = JSON.parse(json_string_response)
-
-# p ruby_hash_response
-# output = File.new("output.txt", "w")
-# output.puts(ruby_hash_response)
