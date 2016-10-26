460c0e2c3d4b4be3a042e206e1815c5a353b7dcc
Scott Dixon <dixon.scottr@gmail.com>
2016-10-25 20:54:54 -0400

    Fix pretty word and phone # format methods

--- restaurant_viewer.rb
+++ restaurant_viewer.rb
@@ -4,16 +4,16 @@ class RestaurantViewer
   def self.display_report(report)
 
     report.critical_flag == "Critical" ? c_flag = " ☠️ " : c_flag = " 👍 "
-
+    # binding.pry
     puts <<-REPORT
     INCIDENT REPORT#{c_flag}:
 
-    #{report.dba}
+    #{make_pretty_word(report.dba)}
     ================
     Grade: #{report.grade}
-    Address: #{report.building} #{report.street}, #{report.zipcode}
-    Borough: #{report.boro}
-    Phone: #{report.phone}
+    Address: #{report.building} #{make_pretty_word(report.street)}, #{report.zipcode}
+    Borough: #{make_pretty_word(report.boro)}
+    Phone: #{format_phone_number(report.phone)}
     ----------------
     Cuisine: #{report.cuisine_description}
     Date of Inspection: #{report.inspection_date}
@@ -24,17 +24,17 @@ class RestaurantViewer
     REPORT
   end
 
-  # def make_pretty_word(ugly_word)
-  #   ugly_word.split.map {|word| word.capitalize}.join(' ')
-  # end
+  def self.make_pretty_word(ugly_word)
+    ugly_word.split.map {|word| word.capitalize}.join(' ')
+  end
 
-  # def format_phone_number(phone)
-  #   if phone.match(/\d{10}/)
-  #     area_code = phone.slice(0..2)
-  #     middle_nums = phone.slice(3..5)
-  #     end_nums = phone.slice(6..9)
-  #     [area_code, middle_nums, end_nums].join('-')
-  #   end
-  # end
+  def self.format_phone_number(phone)
+    if phone.match(/\d{10}/)
+      area_code = phone.slice(0..2)
+      middle_nums = phone.slice(3..5)
+      end_nums = phone.slice(6..9)
+      [area_code, middle_nums, end_nums].join('-')
+    end
+  end
 
 end
